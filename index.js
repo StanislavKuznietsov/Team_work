f01e48420f6446b7d45eee77b71217f1f7d98ab4
VladyslavMartynov <vladyslav.martynov10@gmail.com>
2021-08-17 13:33:44 +0300

    new Js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Hi again =)
+"use strict"
+// function fib(n) {
+//     return (n <= 1) ? 1 : fib(n-1) + fib(n-2)
+// }
 
-function fib(n) {
-    return (n <= 1) ? 1 : fib(n-1) + fib(n-2)
-}
-
-for (let i = 0; i <= 42; i++){
-    console.log(i, fib(i))
-}
+// for (let i = 0; i <= 42; i++){
+//     console.log(i, fib(i))
+// }
 
 function pascalTriangle(row, col) {
     if (col === 0) {
@@ -17,3 +17,26 @@ function pascalTriangle(row, col) {
         return pascalTriangle(row - 1, col) + pascalTriangle(row - 1, col - 1);
     }
 }
+
+class Pascal{
+
+    constructor(row ,col){
+        this.row = row;
+        this.col = col; 
+    }
+
+    pascalTriangle(row,col){
+        if (col === 0) {
+            return 1;
+        } else if (row === 0) {
+            return 0;
+        } else {
+            return pascalTriangle(row - 1, col) + pascalTriangle(row - 1, col - 1);
+        }
+    }
+}
+
+const pascal = new Pascal(2,3);
+console.log(pascal.pascalTriangle(2,5));
+console.log(pascal);
+
